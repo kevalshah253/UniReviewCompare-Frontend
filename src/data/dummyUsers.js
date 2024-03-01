2fe08f41af76dec6ded0b6ee4c936c2841a5d6be
edielam <edguard738@gmail.com>
2024-03-01 11:05:33 -0500

    login

---
+++ src/data/dummyUsers.js
@@ -0,0 +1,19 @@
+const dummyUsers = [
+    {
+      _id: 1,
+      name: "John",
+      surname: "Doe",
+      email: "john.doe@example.com",
+      password: "password123",
+    },
+    {
+      _id: 2,
+      name: "Jane",
+      surname: "Smith",
+      email: "jane.smith@example.com",
+      password: "securePass",
+    },
+    // Add more users as needed
+  ];
+  
+  export default dummyUsers;
