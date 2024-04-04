43c6c1d83502abf26ed96133ba0c494f2702dca9
edielam <edguard738@gmail.com>
2024-04-04 10:51:55 -0400

    review and rating

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -128,145 +128,214 @@
 
 // export default QuestionCards;
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import sampleQuestions from "../data/sampleQuestions";
 import { motion } from "framer-motion";
 import fakeUniData from "../data/fakeUni";
 import { useTranslation } from "react-i18next";
 
-const QuestionCards = (props) => {
+const QuestionCards = () => {
   const [value, setValue] = useState([]);
-  const [comp, setComp] = useState({});
+  const [selectedUniversity, setSelectedUniversity] = useState(null);
   const [review, setReview] = useState("");
-  const params = useParams();
+  const [searchTerm, setSearchTerm] = useState("");
   const { t } = useTranslation([]);
 
-  useEffect(() => {
-    fakeUniData.filter(university => university.id == params.universityID)
-      .map(university => {
-        setComp(university);
-      });
-  }, []);
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-  function handleSubmit(array) {
+  const handleSelect = (university) => {
+    setSelectedUniversity(university);
+  };
+
+  const filteredUniversities = fakeUniData.filter((university) =>
+    university.university.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSubmit = (array) => {
     if (array.length < 5) {
-      alert('Rating Failed');
-    }
-    else {
+      alert("Rating Failed");
+    } else {
       const obj = {
-        isim: comp.university,
-        sehir: comp.location,
+        isim: selectedUniversity.university,
+        sehir: selectedUniversity.location,
         oy_1: array[0].rate,
         oy_2: array[1].rate,
         oy_3: array[2].rate,
         oy_4: array[3].rate,
         oy_5: array[4].rate,
-        review: review
-      }
-      // Simulating post request, replace it with actual logic
-      console.log(obj); // Log the object to check if data is correct
-      alert('Rating is Successful');
-      // window.location.replace(`/home`);
+        review: review,
+      };
+      console.log(obj); // Simulating post request, replace it with actual logic
+      alert("Rating is Successful");
     }
-  }
+  };
 
   return (
     <>
       <div className="container mt-3">
-        <div className="row justify-content-center">
-          {sampleQuestions.map((question, index) => (
-            <div className="col-lg-11 col-md-9 col-sm-10 col-12 mb-3" key={question.id}>
-              <div className="card" style={{ backgroundColor: "#eef3f4", borderRadius: "15px" }}>
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <img
-                      src={question.img}
-                      alt="University Logo"
-                      className="img-fluid rounded-start"
-                      style={{ width: "300px", height: "230px", border: "1.5px solid black", borderRadius: "10px", marginLeft: "5px" }}
-                    />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body" style={{ textAlign: "justify" }}>
-                      <h5 className="card-title" style={{ color: "#1976D2" }}>{question.questionCategory}</h5>
-                      <h6 className="card-subtitle mb-2">{question.question}</h6>
-                    </div>
-                    <div className="card-body">
-                      <h6 className="card-subtitle mb-2" style={{ color: "#1976D2" }}>{question.subTitle}</h6>
-                      <div className="rating">
-                        {[...Array(5)].map((_, i) => (
-                          <span
-                            key={i}
-                            className={`star ${i < value[index]?.rate ? 'filled' : ''}`}
-                            onClick={() => {
-                              if (value[index]) {
-                                let newArr = [...value];
-                                newArr[index] = { id: index, rate: i + 1 };
-                                setValue(newArr);
-                              } else {
-                                setValue([...value, { id: index, rate: i + 1 }]);
-                              }
-                            }}
-                            style={{ cursor: "pointer" }}
+        <div className="row justify-content-center mb-3">
+          <div className="col-lg-6">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder={t("University")}
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+              <button
+                className="btn btn-outline-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {t("Select University")}
+              </button>
+              <ul className="dropdown-menu dropdown-menu-end">
+                {filteredUniversities.map((university) => (
+                  <li key={university.id}>
+                    <a
+                      className="dropdown-item"
+                      href="#"
+                      onClick={() => handleSelect(university)}
+                    >
+                      {university.university}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {selectedUniversity && (
+          <div>
+            <div className="row justify-content-center">
+              {sampleQuestions.map((question, index) => (
+                <div
+                  className="col-lg-11 col-md-9 col-sm-10 col-12 mb-3"
+                  key={question.id}
+                >
+                  <div
+                    className="card"
+                    style={{ backgroundColor: "#eef3f4", borderRadius: "15px" }}
+                  >
+                    <div className="row g-0">
+                      <div className="col-md-4">
+                        <img
+                          src={question.img}
+                          alt="University Logo"
+                          className="img-fluid rounded-start"
+                          style={{
+                            width: "300px",
+                            height: "230px",
+                            border: "1.5px solid black",
+                            borderRadius: "10px",
+                            marginLeft: "5px",
+                          }}
+                        />
+                      </div>
+                      <div className="col-md-8">
+                        <div
+                          className="card-body"
+                          style={{ textAlign: "justify" }}
+                        >
+                          <h5
+                            className="card-title"
+                            style={{ color: "#1976D2" }}
                           >
-                            &#9733;
-                          </span>
-                        ))}
+                            {question.questionCategory}
+                          </h5>
+                          <h6 className="card-subtitle mb-2">
+                            {question.question}
+                          </h6>
+                        </div>
+                        <div className="card-body">
+                          <h6
+                            className="card-subtitle mb-2"
+                            style={{ color: "#1976D2" }}
+                          >
+                            {question.subTitle}
+                          </h6>
+                          <div className="rating">
+                            {[...Array(5)].map((_, i) => (
+                              <span
+                                key={i}
+                                className={`star ${
+                                  i < value[index]?.rate ? "filled" : ""
+                                }`}
+                                onClick={() => {
+                                  if (value[index]) {
+                                    let newArr = [...value];
+                                    newArr[index] = { id: index, rate: i + 1 };
+                                    setValue(newArr);
+                                  } else {
+                                    setValue([...value, { id: index, rate: i + 1 }]);
+                                  }
+                                }}
+                                style={{ cursor: "pointer" }}
+                              >
+                                &#9733;
+                              </span>
+                            ))}
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
+              ))}
+            </div>
+
+            <div className="row justify-content-center mb-3">
+              <div className="col-lg-6">
+                <div className="mb-3">
+                  <label htmlFor="reviewInput" className="form-label">
+                    {t("CompanyPage.review.label")}
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="reviewInput"
+                    rows="3"
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                  ></textarea>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
 
-        <div className="row justify-content-center mb-3">
-          <div className="col-lg-6">
-            <div className="mb-3">
-              <label htmlFor="reviewInput" className="form-label">
-                {t("CompanyPage.review.label")}
-              </label>
-              <textarea
-                className="form-control"
-                id="reviewInput"
-                rows="3"
-                value={review}
-                onChange={(e) => setReview(e.target.value)}
-              ></textarea>
+            <div className="row justify-content-center mb-3">
+              <div className="col">
+                <motion.div
+                  className="animatable"
+                  whileHover={{
+                    scale: 1.1,
+                    transition: { duration: 0.3 },
+                  }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  <button
+                    onClick={() => {
+                      handleSubmit(value);
+                    }}
+                    className="btn btn-primary"
+                    style={{
+                      padding: "10px",
+                      backgroundColor: "#1e88e5",
+                      color: "#fff",
+                    }}
+                  >
+                    {t("Submit Review")}
+                  </button>
+                </motion.div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="row justify-content-center mb-3">
-          <div className="col">
-            <motion.div
-              className="animatable"
-              whileHover={{
-                scale: 1.1,
-                transition: { duration: 0.3 }
-              }}
-              whileTap={{ scale: 0.9 }}
-            >
-              <button
-                onClick={() => {
-                  handleSubmit(value);
-                }}
-                className="btn btn-primary"
-                style={{
-                  padding: "10px",
-                  backgroundColor: "#1e88e5",
-                  color: "#fff"
-                }}
-              >
-                {t("Submit Review")}
-              </button>
-            </motion.div>
-          </div>
-        </div>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default QuestionCards;
